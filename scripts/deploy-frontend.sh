1b394fe0e8b4a1a5bfa943e2ea7312a3291ce6fb
JohnSotonwa <johnsotonwa@gamil.com>
2026-01-27 22:40:27 +0000

    Fix some syntax errors

--- scripts/deploy-frontend.sh
+++ scripts/deploy-frontend.sh
@@ -4,7 +4,7 @@ set -e
 # Deploy React frontend to S3 and invalidate CloudFront cache
 
 echo "Syncing React build to S3 bucket: $S3_BUCKET_NAME..."
-aws s3 sync frontend/build s3://$S3_BUCKET_NAME --delete
+aws s3 sync frontend/dist s3://$S3_BUCKET_NAME --delete
 
 echo "Invalidating CloudFront cache: $CLOUDFRONT_DIST_ID..."
 aws cloudfront create-invalidation \
